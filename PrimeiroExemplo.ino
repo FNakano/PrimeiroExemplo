160b60743864addc782637fc1a550238c1ed02a9
FNakano <fnakano1970@gmail.com>
2015-09-24 22:22:16 -0300

    Comentário
    Acrescentado comentário sobre o que são setup( ) e loop( )

--- PrimeiroExemplo.ino
+++ PrimeiroExemplo.ino
@@ -1,3 +1,9 @@
+// O sketch minimo do arduino tem dois metodos: setup() e loop()
+// setup eh executado apenas uma vez quando o arduino recebe
+// um reset.
+// loop() eh repetido "eternamente", ou seja, depois de um
+// reset, o arduíno executa setup(), loop(), loop(), loop(),...
+
 void setup() {
   // put your setup code here, to run once:
 
